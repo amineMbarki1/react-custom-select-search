b30e70fa2aa1593849ed711c0438cf1f260b8d61
Amine <aminembarki1994@outlook.com>
2023-06-23 19:43:45 +0100

    Updates

--- assets/preview-ffe7c7b6.js
+++
@@ -1,2 +0,0 @@
-/* empty css                     */const o={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}}};export{o as default};
-//# sourceMappingURL=preview-ffe7c7b6.js.map
